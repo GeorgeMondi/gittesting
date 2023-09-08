a9cc606e73787aa31880a331619933fcd975496b
GeorgeMondi <george.marolix@gmail.com>
2023-09-08 10:20:35 -0400

    python

---
+++ python.py
@@ -0,0 +1,27 @@
+print("Hello World")
+
+#while 
+a = 4  
+counter = 0  
+while counter < 3:  
+    a = a + 1  
+    print(a)  
+    counter = counter + 1
+
+#for
+word = "Python"  
+for each_char in word:  
+    print(each_char)
+
+for number in range(5, 8):  
+    print(number)
+
+
+
+
+
+
+
+
+
+
