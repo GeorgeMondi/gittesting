7d1cb651599269f01198b3628a8acae155c765fd
GeorgeMondi <george.marolix@gmail.com>
2023-09-08 13:46:01 -0400

    loops

--- python.py
+++ python.py
@@ -17,6 +17,48 @@ for number in range(5, 8):
     print(number)
 
 
+i = 0
+while i < 5:
+    i += 1
+    if i == 2:
+        print("Skipping 2")
+        continue
+    elif i == 4:
+        print("Break at 4")
+        break
+    else:
+        pass
+    print("Current value of i:", i)
+
+
+for j in range(1, 6):
+    if j == 3:
+        print("Skipping 3")
+        continue
+    elif j == 5:
+        print("Breaking at 5")
+        break
+    else:
+        pass
+    print("Current value of j:", j)  
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
